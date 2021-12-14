ccedab7b0863d70be07e8dbd94c5bb1f00706972
LaviniaUnder <lavinia.underbougth@outlook.com>
2021-12-14 14:07:05 -0300

    Create Exercicio media.py
    Lesson 1 - Creating Variables, Input & Output

---
+++ Exercicio media.py
@@ -0,0 +1,7 @@
+nome = input("digite seu nome: ")
+matricula =  int(input("digite a sua matricula: "))
+nota1 = float(input("Digite a sua primeira nota:"))
+nota2 = float(input("Digite a sua segunda nota:"))
+media = (nota1 + nota2) / 2
+
+print(nome, ": ", matricula, "\n", "Sua média final é: ", media)
